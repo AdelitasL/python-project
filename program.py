a45d83e6c9fc727416e4824ad02b164ecf1fffd0
AdelitasL <nkazakovofficial@gmail.com>
2020-11-03 11:49:43 -0600

    new branch

--- program.py
+++ program.py
@@ -1,7 +1,7 @@
 def main(msg):
-//another comment
-//differentcomment
-//printamessage
+//No longer need comments
     print(msg)
 
+def secondary():
+    print("New function")
 main("Hello People")
